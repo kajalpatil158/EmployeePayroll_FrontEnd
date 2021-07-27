df5d160e59e81ec394afe51d1e95948728732b04
Kajal <kajalpatil158@gmail.com>
2021-07-27 13:06:53 +0530

    feat : employee payroll front end data

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Multiple from './Multiples';
+
+function App() {
+  return (
+    <div className="App">
+    <Multiple />
+    </div>
+  );
+}
+
+export default App;
