7af9201ec40df41a9296cadeeebee3393e234ed7
Kajal <kajalpatil158@gmail.com>
2021-07-28 18:16:20 +0530

    feat: initial values is added for registration

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -10,10 +10,18 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
  * @return Registration Page 
  */
 
- const Register = () => {
+const Register = () => {
     const paperStyle = { padding: 20, height: '80vh', width: 200, margin: '20px auto' }
     const avtarStyle = { backgroundColor: 'red' }
     const btnStyle = { margin: '8px 0' }
+    const initialValues = {
+        FirstName: '',
+        LastName: '',
+        Email: '',
+        Password: '',
+        ConfirmPassword: '',
+
+    }
     return (
         <Grid align='center'>
             <Paper elevation={15} style={paperStyle}>
@@ -21,36 +29,36 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
                     <h2>Register</h2>
                 </Grid>
                 <Formik initialValues={initialValues}
-                    validationSchema={validationSchema}
-                    onSubmit={onSubmit}>
-                    {(props) =>
-                    (
-                <TextField label='FirstName' placeholder='Enter FirstName' fullwidth requied />
-                <TextField label='LastName' placeholder='Enter LastName' fullwidth requied />
-                <TextField label='Email' placeholder='Enter EmailId' fullwidth requied />
-                <TextField label='Password' placeholder='Enter Password' type='password' fullwidth requied />
-                <TextField label='ConfirmPassword' placeholder='Enter ConfirmPassword' type='password' fullwidth requied />
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            name="checkedB"
-                            color="primary"
-                        />
-                    }
-                    label="Remember me"
-                />
-                <Button type='submit' color='primary' style={btnStyle} fullWidth variant='contained'>Sign In</Button>
-                <Typography>
-                    <Link href="#" >
-                        Forgot Password ?
-                    </Link>
-                </Typography>
+                    >
+                    {(props) => (
+                        <Form>
+                            <Field as={TextField} label='FirstName' name='FirstName' placeholder='Enter FirstName' fullwidth requied />
+                            <Field as={TextField} label='LastName' name='LastName' placeholder='Enter LastName' fullwidth requied />
+                            <Field as={TextField} label='Email' name='Email' placeholder='Enter EmailId' fullwidth requied />
+                            <Field as={TextField} label='Password' name='Password' placeholder='Enter Password' type='password' fullwidth requied />
+                            <Field as={TextField} label='ConfirmPassword' name='ConfirmPassword' placeholder='Enter ConfirmPassword' type='password' fullwidth requied />
+                            <FormControlLabel
+                                control={
+                                    <Field as={Checkbox}
+                                        name="checkedB"
+                                        color="primary"
+                                    />
+                                }
+                                label="Remember me"
+                            />
+                            <Button type='submit' color='primary' style={btnStyle} fullWidth variant='contained'>Sign In</Button>
+                            <Typography>
+                                <Link href="#" >
+                                    Forgot Password ?
+                                </Link>
+                            </Typography>
 
-                <Typography> Do you have an account ?
-                    <Link href="#" >
-                        Sign Up
-                    </Link>
-                </Typography>)}
+                            <Typography> Do you have an account ?
+                                <Link href="#" >
+                                    Sign Up
+                                </Link>
+                            </Typography>
+                        </Form>)}
                 </Formik>
             </Paper>
         </Grid>
