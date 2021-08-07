382555f8c0ae0463a12e0f98d600902ec4cc20b4
Kajal <kajalpatil158@gmail.com>
2021-08-07 18:49:16 +0530

    refactor: appbar code

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,147 +1,40 @@
-
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import { Link } from 'react-router-dom';
-import IconButton from '@material-ui/core/IconButton';
-import List from '@material-ui/core/List';
-import Button from '@material-ui/core/Button';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
-import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import AddBoxOutlinedIcon from '@material-ui/icons/AddBoxOutlined';
-import ListAltOutlinedIcon from '@material-ui/icons/ListAltOutlined';
-import EditAttributesOutlinedIcon from '@material-ui/icons/EditAttributesOutlined';
-import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined';
-
-
-const drawerWidth = 80;
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-  },
-  drawer: {
-    [theme.breakpoints.up('sm')]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-  },
- 
-  appBar: {
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-    },
-  },
-  toolbar: theme.mixins.toolbar,
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  content: {
     flexGrow: 1,
-    padding: theme.spacing(3),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
   },
 }));
 
-function Dashboard(props) {
-  const { window } = props;
+export default function ButtonAppBar() {
   const classes = useStyles();
-  const theme = useTheme();
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
-  const drawer = (
-    <div>
-      <div className={classes.toolbar} />
-      <Divider />
-      <List>
-      <ListItem  button component={Link} to={'/dash'}>
-      <ListItemText primary="List" />
-      </ListItem>
-      </List>
-      <List>
-      <ListItem  button component={Link} to={'/dash'}>
-      <ListItemText primary="Add" />
-      </ListItem>
-      </List>
-      <List>
-      <ListItem  button component={Link} to={'/dash'}>
-      <ListItemText primary="Edit" />
-      </ListItem>
-      </List>
-      <List>
-      <ListItem  button component={Link} to={'/dash'}>
-      <ListItemText primary="Delete" />
-      </ListItem>
-      </List>
-      
-    </div>
-  );
-
-  const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
     <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
+      <AppBar position="static">
         <Toolbar>
-        <IconButton edge="start"  color="inherit" aria-label="menu">
-      .  <MenuIcon />
-      </IconButton>
-          <Typography variant="h6" noWrap>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
             Employee Payroll App
           </Typography>
           <Button color="inherit">Log Out</Button>
         </Toolbar>
       </AppBar>
-      <nav className={classes.drawer} >
-        <Hidden smUp implementation="css">
-          <Drawer
-            container={container}
-            variant="temporary"
-            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
-          >
-            {drawer}
-          </Drawer>
-        </Hidden>
-        <Hidden xsDown implementation="css">
-          <Drawer
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            variant="permanent"
-            open
-          >
-            {drawer}
-          </Drawer>
-        </Hidden>
-      </nav>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        
-      </main>
     </div>
   );
 }
-
-export default Dashboard;
