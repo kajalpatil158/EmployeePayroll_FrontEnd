f745fb56b1d99685f376b91281c9106fe56bb1db
Kajal <kajalpatil158@gmail.com>
2021-08-09 17:53:22 +0530

    feat: notification and controls is added in components

---
+++ src/components/controls/notifications.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Snackbar, makeStyles } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
+
+const useStyles = makeStyles(theme => ({
+    root:{
+        top:theme.spacing(1)
+    }
+}))
+
+export default function Notification(props) {
+
+    const classes = useStyles()
+    const { notify, setNotify } = props
+
+    const handleClose = (event, reason) => {
+        setNotify({
+            ...notify,
+            isOpen: false
+        })
+    }
+
+    return (
+        <Snackbar 
+            className={classes.root}
+            open={notify.isOpen}
+            autoHideDuration={3000}
+            anchorOrigin={{vertical:'top', horizontal: 'center'} }
+            onClose={handleClose}
+        >
+            <Alert 
+                severity={notify.type}
+                onClose={handleClose}
+            >
+                {notify.message}
+            </Alert>
+        </Snackbar>
+    )
+}
