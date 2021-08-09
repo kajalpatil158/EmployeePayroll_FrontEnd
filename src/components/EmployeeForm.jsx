4f9511aa4031dda0f5dcad4a044181ccc2cbca00
Kajal <kajalpatil158@gmail.com>
2021-08-09 15:10:56 +0530

    feat: useform is created into employeeform folder

---
+++ src/components/EmployeeForm.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+
+const initialValues = {
+    _id:'',
+    firstName: '',
+    lastName: '',
+    email: '',
+    department: '',
+    salary: ''
+}
+
+export default function EmployeeForm(props) {
+    return(
+        <Form>
+        
+        </Form>
+    )
+   
+}
