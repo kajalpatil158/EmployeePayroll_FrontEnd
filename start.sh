fe3d3ee22cd7bcdff1f16015e8ea1ce9dc94a2d6
Kajal <kajalpatil158@gmail.com>
2021-08-23 19:16:39 +0530

    refator: code

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 
-cd /home/ubuntu/Employee_Payroll_App_FrontEnd
+cd  /home/ubuntu/EmployeePayroll_Frontend
 working_directory=$(pwd)
 echo "Present working directory = $working_directory"
-npm i 
+npm i
 echo "Installing packages"
 npm run build
 echo "build success!"
-npm start 
+npm start
